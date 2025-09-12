cd756e165631d23e623820f15ed09bb97db8bed5
chris985 <christianlonardoni@gmail.com>
2025-09-12 10:03:39 -0300

    commit

--- models.py
+++ models.py
@@ -45,6 +45,40 @@ class Produtos(Base):
         return dados_user
 
 
+# Dados da Lista
+class Cadastro_clientes(Base):
+    __tablename__ = 'Produtos'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(100), nullable=False, index=True)
+    email = Column(String(100), nullable=False, index=True)
+    cpf = Column(String(100), nullable=False, index=True)
+
+    # Representação de Classe
+    def __repr__(self):
+        return '<User: {} {} {}>'.format(self.nome,
+                                          self.email,
+                                          self.cpf,
+                                          )
+
+    # Função para Salvar no Banco
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+
+    # Função para Deletar no Banco
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
+
+    # Coloca os Dados na Tabela
+    def serialize(self):
+        dados_user = {
+            'nome': self.nome,
+            'categoria': self.categoria,
+        }
+        return dados_user
+
+
 # Método para criar Banco
 def init_db():
     Base.metadata.create_all(bind=engine)
