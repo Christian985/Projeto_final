104b5bd7a06796a38a18f793b5f2a876a762fa43
chris985 <christianlonardoni@gmail.com>
2025-11-14 09:23:32 -0300

    inicio aplicação web

--- app.py
+++ app.py
@@ -5,30 +5,36 @@ app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = "03050710"
 jwt = JWTManager(app)
 
+
 # Renderiza a Primeira Página/Início
 @app.route('/')
 def index():
     return render_template('template.html')
+
 
 # Renderiza o Cadastro de Pessoas
 @app.route('/pessoas', methods=['GET', 'POST'])
 def listar_clientes():
     return render_template('cadastro_pessoas.html')
 
+
 # Renderiza o Cadastro de Produtos
 @app.route('/produtos', methods=['GET', 'POST'])
 def listar_produtos():
     return render_template('cadastro_produtos.html')
+
 
 # Renderiza o Cadastro de Vendas
 @app.route('/vendas', methods=['GET', 'POST'])
 def listar_vendas():
     return render_template('cadastro_vendas.html')
 
+
 # Renderiza o Cadastro de Categoria
 @app.route('/categorias', methods=['GET', 'POST'])
 def listar_categorias():
     return render_template('cadastro_categorias.html')
+
 
 # Inicia
 if __name__ == '__main__':
