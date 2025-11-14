281f34cc4563e1a20881968cdf729885c7dda14f
chris985 <christianlonardoni@gmail.com>
2025-11-14 11:02:57 -0300

    inicio aplicação web

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, render_template, request, redirect, url_for, flash
 from flask_jwt_extended import create_access_token, jwt_required, JWTManager
 
+import routes
+
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = "03050710"
 jwt = JWTManager(app)
@@ -12,25 +14,31 @@ def index():
     return render_template('template.html')
 
 
-# Renderiza o Cadastro de Pessoas
-@app.route('/pessoas', methods=['GET', 'POST'])
+# Renderiza a Lista de Pessoas
+@app.route('/pessoas')
+def listar_clientes():
+    return render_template('cadastro_pessoas.html')
+
+# Renderiza a Lista de Pessoas
+@app.route('/pessoas')
 def listar_clientes():
     return render_template('cadastro_pessoas.html')
 
 
-# Renderiza o Cadastro de Produtos
-@app.route('/produtos', methods=['GET', 'POST'])
+# Renderiza a Lista de Produtos
+@app.route('/produtos', methods=['GET'])
 def listar_produtos():
+    vendas = routes.get_produtos()
     return render_template('cadastro_produtos.html')
 
 
-# Renderiza o Cadastro de Vendas
+# Renderiza a Lista de Vendas
 @app.route('/vendas', methods=['GET', 'POST'])
 def listar_vendas():
     return render_template('cadastro_vendas.html')
 
 
-# Renderiza o Cadastro de Categoria
+# Renderiza a Lista de Categoria
 @app.route('/categorias', methods=['GET', 'POST'])
 def listar_categorias():
     return render_template('cadastro_categorias.html')
